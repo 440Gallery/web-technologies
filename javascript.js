bd32b143e7186c35bf26b8dc20e3fb474e007a52
unknown <btanusha993@gmail.com>
2026-06-02 21:51:41 +0530

    primitive and non primitive

---
+++ javascript.js
@@ -0,0 +1,26 @@
+console.log("javascript loaded");
+
+var age=900000000000000000n;
+console.log(typeof age);
+
+var name="dev";
+console.log(typeof name);
+
+var student=true;
+console.log(typeof student);
+
+var id;
+console.log(typeof id);
+
+var student1=null;
+console.log(student1);
+
+var hobbies=["drawing","painting","sketching"];
+console.log(typeof hobbies[0]);
+
+var person={
+    name:"virat",
+    age:35,
+    qualification:"degree"
+}
+console.log(typeof person);
